b3cef9d04c342fd96c1176bebf9861645fdae74d
pedrou2000 <urbina2000pedro@gmail.com>
2024-06-22 16:48:32 +0100

    Using classes for nodes instead of methods

---
+++ src/1-Book_Summarizer/prompts.py
@@ -0,0 +1,10 @@
+##### Agent Section Summarizer #####
+SECTION_SUMMARIZER_SYSTEM_PROMPT = """
+    You are an expert summarizer. You are an agent working in summarizing a single chapter in a book, 
+    which will then be used to create a summary of the entire book. You are tasked with summarizing the chapter.
+"""
+
+SECTION_SUMMARIZER_USER_PROMPT = """
+    {section}\n Summary:
+"""
+
